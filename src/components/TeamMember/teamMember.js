2e62e409a6e801bfaf42ab3e12d24c2ed9f297bc
andrico <andrico1234@yahoo.co.uk>
2019-01-12 16:15:29 +0000

    make progress on about the team page

--- src/components/TeamMember/teamMember.js
+++ src/components/TeamMember/teamMember.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const TeamMember = ({ profilePic }) => {
   return (
-    <div>
-      <img src={profilePic} />
-    </div>
-  ) 
+    <MemberContent>
+      <ContentWrapper>
+        <Image src={profilePic} />
+        <Description>
+          <p>Hey there</p>
+        </Description>
+      </ContentWrapper>
+    </MemberContent>
+  )
 }
 
 export default TeamMember
+
+const MemberContent = styled.div`
+  text-align: center;
+  width: 350px;
+`
+
+const ContentWrapper = styled.div`
+  position: relative;
+  margin: 0 auto;
+  width: 200px;
+`
+
+const Image = styled.img`
+  transition: all 0.2s linear;
+  width: 200px;
+
+  &:hover {
+    transform: scale(1.1);
+    opacity: 0.3;
+  }
+`
+
+const Description = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+`
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
-import { David, Eva, Jacks, Jaime, Kirstie, Paul, Sonja } from '../images/profile-pictures'
+import {
+  David,
+  Eva,
+  Jacks,
+  Jaime,
+  Kirstie,
+  Paul,
+  Sonja,
+} from '../images/profile-pictures'
 import TeamMember from '../components/TeamMember/teamMember'
 import Layout from '../components/Layout/layout'
 import SEO from '../components/Seo/seo'
 
-const imagesArray = [
-  David, Eva, Jacks, Jaime, Kirstie, Paul, Sonja,
-];
+const imagesArray = [David, Eva, Jacks, Jaime, Kirstie, Paul, Sonja]
 
 const TeamPage = () => {
   return (
     <Layout>
       <SEO title="Meet the team" />
       <StyledHeader>Meet the team</StyledHeader>
-      {
-        imagesArray.map(image => (
-          <TeamMember
-            profilePic={image}
-          />
-        ))
-      }
+      <MemberContainer>
+        {imagesArray.map(image => (
+          <TeamMember profilePic={image} />
+        ))}
+      </MemberContainer>
     </Layout>
   )
 }
@@ -30,4 +34,10 @@ export default TeamPage
 const StyledHeader = styled.h1`
   color: white;
   padding-left: 16px;
+`
+
+const MemberContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `

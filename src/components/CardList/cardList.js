2f4bfc27aa3c81da30b272d932986fe2a0399f18
andrico <andrico1234@yahoo.co.uk>
2019-01-26 17:38:35 +0000

    remove console.log from here

--- src/components/CardList/cardList.js
+++ src/components/CardList/cardList.js
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 import Card from './Card/card';
 
 const CardList = ({ data }) => {
-  console.log('data', data);
   return (
     <StyledCardList>
       {data.map(({ node }) => (

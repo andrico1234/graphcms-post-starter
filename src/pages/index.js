f4944694fb8a5f5cf681489f856d4c9298268343
andrico <andrico1234@yahoo.co.uk>
2019-01-26 15:18:50 +0000

    code style improvmenets

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,32 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 import Layout from '../components/Layout/layout'
-import Heading from '../components/Heading/heading';
+import Heading from '../components/Heading/heading'
 import EventListing from '../components/EventListing/eventListing'
 import GardenImage from '../images/garden.jpg'
 
 const data = [
   {
     node: {
-      id: 123
-    }
+      id: 123,
+    },
   },
   {
     node: {
       id: 234,
-    }
+    },
   },
   {
     node: {
       id: 325,
     },
   },
-];
+]
 
 const IndexPage = () => (
   <Layout>
-    <Image src={GardenImage} alt="Photo by Scott Webb on Unsplash"/>
+    <Image src={GardenImage} alt="Photo by Scott Webb on Unsplash" />
     <Heading title="Garden Party" />
     <EventListing data={data} />
   </Layout>
